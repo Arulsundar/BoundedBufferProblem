c62da04c77b1ebab72b8fff2275e5247b5f14946
Arulsundar <96573710+Arulsundar@users.noreply.github.com>
2022-02-17 22:54:31 +0530

    Update MyArrayBlockingQueue.java

--- BoundedBufferProblem/src/com/queue/MyArrayBlockingQueue.java
+++ BoundedBufferProblem/src/com/queue/MyArrayBlockingQueue.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MyArrayBlockingQueue implements Serializable {
-	private static final long serialVersionUID = 1L;
+// 	private static final long serialVersionUID = 1L;
 	private final Integer[] queue ;
     private final int capacity ;
     //left and right never points to the same position ever.
