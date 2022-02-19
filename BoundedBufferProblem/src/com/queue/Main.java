cc80ce5a19528ae76c845c1c91c960e8147f82f4
gnana-pt4726 <gnana-pt4726@gnana-pt4726.tsi.zohocorpin.com>
2022-02-19 15:58:06 +0530

    commit

--- BoundedBufferProblem/src/com/queue/Main.java
+++ BoundedBufferProblem/src/com/queue/Main.java
@@ -1,59 +1,57 @@
 package com.queue;
 
-import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-     
 
-    public static void main(String[] args) throws InterruptedException {
-        WrapperQueue q = new WrapperQueue(5) ;
-        final Runnable producer=() ->
-	      {  
-	    	  
-	    	  while(true)
-	    	  {
-	    		  
-	    		  try {
-	    			  int r=(int) (Math.random()*10);
-	    			  System.out.println("producing :"+r);
-					  q.put(r);
-					  r++;
-					  TimeUnit.SECONDS.sleep(1);
+	public static void main(String[] args) throws InterruptedException {
+		WrapperQueue q = new WrapperQueue(5);
+		AtomicInteger num=new AtomicInteger();
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+		final Runnable producer = () -> {
+			while (true) {
+
+				try {
+//					int r = (int) (Math.random() * 10);
+					int r=num.getAndIncrement();
+					System.out.println("producing :" + r);
+					q.put(r);
+					TimeUnit.SECONDS.sleep(1);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-	    	  }
-	      };
-	      new Thread(producer).start();
-	      new Thread(producer).start();
-	      final Runnable consumer=() ->
-	      { 
-	    	  while(true)
-	    	  {
-	    		  
-	    		  try {
-	    			  Integer val=q.take();
-	    			  if(val!=null)
-	    			   System.out.println("consuming :"+ val);				  
-	    			  TimeUnit.SECONDS.sleep(1);
-					  
-				} catch (ClassNotFoundException | IOException | InterruptedException e) {
+			}
+		};
+//	      new Thread(producer).start();
+		final Runnable consumer = () -> {
+			while (true) {
+
+				try {
+					Integer val = q.take();
+					System.out.println("consuming :" + val);
+					TimeUnit.SECONDS.sleep(2);
+
+				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-	    	  }
-	      };
-	      new Thread(consumer).start();
-	      while(true)
-	      {
-	    	   new Thread(producer).start();
-	    	   Thread.sleep(1000);
-	           new Thread(consumer).start();
-	           Thread.sleep(1000);
-	      }
-		
-	
-
-    }
+			}
+		};
+//	      new Thread(consumer).start();
+//		while (true) {
+//			new Thread(producer).start();
+//			Thread.sleep(1000);
+//			new Thread(consumer).start();
+//			Thread.sleep(1000);
+//		}
+		while(true)
+		{
+			pool.execute(producer);
+			Thread.sleep(1000);
+			pool.execute(consumer);
+			Thread.sleep(1000);
+		}
+	}
 }
--- BoundedBufferProblem/src/com/queue/MyArrayBlockingQueue.java
+++ BoundedBufferProblem/src/com/queue/MyArrayBlockingQueue.java
@@ -1,57 +1,114 @@
 package com.queue;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MyArrayBlockingQueue implements Serializable {
 	private static final long serialVersionUID = 1L;
-	private final Integer[] queue;
-	private final int capacity;
-	// left and right never points to the same position ever.
-	private AtomicInteger left = new AtomicInteger(-1);
-	private AtomicInteger right = new AtomicInteger(0);
-	private final AtomicInteger size = new AtomicInteger();
+	private final Integer[] queue ;
+    private final int capacity ;
+    //left and right never points to the same position ever.
+    private int left = -1 ;
+    private int right = 0 ;
+    private final AtomicInteger size = new AtomicInteger() ;
 
-	public MyArrayBlockingQueue(int capacity) {
-		this.capacity = capacity;
-		this.queue = new Integer[capacity];
-	}
+    private final ReentrantLock putLock = new ReentrantLock() ;
+    private final Condition notFull = putLock.newCondition() ;
 
-	private void addToLast(Integer e) {
-		queue[right.get()] = e;
-		right.set((right.get() + 1) % capacity);
-		size.incrementAndGet();
-	}
+    private final ReentrantLock takeLock = new ReentrantLock() ;
+    private final Condition notEmpty = takeLock.newCondition() ;
+    
+    private final ExecutorService pool = Executors.newCachedThreadPool();
+    
+    public MyArrayBlockingQueue(int capacity) {
+        this.capacity = capacity ;
+        this.queue = new Integer[capacity] ;
+    }
+    
 
-	private Integer removeFromFront() {
-		left.set((left.get() + 1) % capacity);
-		Integer polled = queue[left.get()];
-		size.decrementAndGet();
-		return polled;
-	}
+    private void addToLast(Integer e) {
+        queue[right] = e;
+        right = (right + 1) % capacity;
+        size.incrementAndGet();
+    }
 
-	public void put(Integer e) {
-		while (queueFull())
-			;
-		addToLast(e);
-	}
+    private Integer removeFromFront() {
+        left = (left + 1) % capacity ;
+        Integer polled = queue[left] ;
+        size.decrementAndGet() ;
+        return polled ;
+    }
 
-	public Integer take() {
-		while (queueEmpty())
-			;
-		Integer polled = removeFromFront();
-		return polled;
-	}
+    private void signalNotEmpty() {
+        takeLock.lock();
+        try {
+            notEmpty.signal();
+        } finally {
+            takeLock.unlock();
+        }
+    }
 
-	public boolean queueFull() {
-		return (size.get() == capacity);
-	}
+    private void signalNotFull() {
+        putLock.lock() ;
+        try {
+            notFull.signal();
+        } finally {
+            putLock.unlock();
+        }
+    }
 
-	public boolean queueEmpty() {
-		return (size.get() == 0);
-	}
+    public void put(Integer e) {
+        putLock.lock() ;
+        try {
+            while (size.get() == capacity) {
+                notFull.await() ;
+            }
+            addToLast(e) ;
+            pool.execute(this::signalNotEmpty);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex) ;
+        } finally {
+            putLock.unlock() ;
+        }
+    }
 
-	public int size() {
-		return size.get();
+    public Integer take() {
+        takeLock.lock() ;
+        try {
+            while(size.get() == 0) {
+                notEmpty.await();
+            }
+            Integer polled = removeFromFront() ;
+            pool.execute(this::signalNotFull);
+            return polled ;
+        } catch (Exception ex) {
+            throw new RuntimeException(ex) ;
+        } finally {
+            takeLock.unlock();	
+        }
+    }
+    public boolean queueFull()
+    {
+    	return (size.get()==capacity);
+    }
+    public boolean queueEmpty()
+    {
+    	return (size.get()==0);
+    }
+    public int size()
+    {
+    	return size.get();
+    }
+
+	@Override
+	public String toString() {
+		return "MyArrayBlockingQueue{" +
+				"queue=" + Arrays.toString(queue) +
+				'}';
 	}
 }
