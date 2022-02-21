ca3db238c5e44ee23f4bfac10feb8a1fa814cec0
arv2k1 <76536617+arv2k1@users.noreply.github.com>
2022-02-21 12:26:53 +0530

    Timer changes

--- BoundedBufferProblem/src/com/queue/Timer.java
+++ BoundedBufferProblem/src/com/queue/Timer.java
@@ -1,6 +1,8 @@
 package com.queue;
 
 public class Timer extends WrapperQueue{
+	final AverageComputeUtil putAvg = new AverageComputeUtil();
+	final AverageComputeUtil takeAvg = new AverageComputeUtil();
 	
 	public Timer(int capacity)
 	{
@@ -8,19 +10,51 @@ public class Timer extends WrapperQueue{
 	}
 	public void put(Integer e)
 	{
-		long start=System.currentTimeMillis();
+		final long start=System.nanoTime();
 		super.put(e);
-		long end=System.currentTimeMillis();
-		System.out.println("producing time:"+(start-end));
+		final long end=System.nanoTime();
+		putAvg.add(end - start);
+		System.out.println("Put timer :: "
+			+ putAvg.getCount() + " puts in "
+			+ new BigDecimal(putAvg.getSum()).divide(new BigDecimal(1_000_000_000)) + " seconds"
+		);
 	}
 	public Integer take()
 	{
-		long start=System.currentTimeMillis();
+		final long start=System.nanoTime();
 		Integer val=super.take();
-		long end=System.currentTimeMillis();
-		System.out.println("consuming time:"+(start-end));
+		final long end=System.nanoTime();
+		takeAvg.add(end - start);
+		System.out.println("Take timer :: "
+			+ takeAvg.getCount() + " takes in "
+			+ new BigDecimal(takeAvg.getSum()).divide(new BigDecimal(1_000_000_000)) + " seconds"
+		);
 		return val;
 	}
-	
-
 }
+
+class AverageComputeUtil {
+	private volatile long sum;
+	private volatile int count;
+
+	public synchronized void add(long val) {
+		sum += val;
+		count++;
+	}
+
+	// return double or bigdecimal ? 
+	// public synchronized long getAvg() {
+	// 	if(count == 0)
+	// 		return 0;
+	// 	return sum / count;
+	// }
+
+	public synchronized int getCount() {
+		return count;
+	}
+
+	public synchronized long getSum() {
+		return sum;
+	}
+}
+
